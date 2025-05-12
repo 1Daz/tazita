9131978185e9ae7c55e16828731009ecf28eb63a
Daz <jhofran.daza251@tau.usbmed.edu.co>
2025-05-11 22:58:53 -0500

    Test

---
+++ src/click.py
@@ -0,0 +1,9 @@
+def runClick():
+    while True:
+        print("-"*10, ' TO DO ', "-"*10)
+        print("1. Agregar tarea")
+        print("2. Listar tareas")
+        print("3. Completar tarea")
+        print("4. Salir")
+
+        opcion = input("Selecciona una opción")
