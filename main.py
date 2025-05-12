6287c4a68f7b49a7b15079255a9768e2ce15c722
jhofran.daza251@tau.usbmed.edu.co <PC4CU-18@campus.usb>
2025-05-12 10:58:44 -0500

    Tesssttt

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from src.click import runClick
 
 if __name__ == "__main__":
-    data = {"ID": 1, "Description": "Hacer comida", "Completed": True}
+    data = [
+        {
+            "ID": 1, 
+            "Description": "Hacer comida", 
+            "Completed": True
+        },
+        {
+            "ID": 2,
+            "Description": "Ir a clase",
+            "Completed": False
+        }
+        
+        ]
     print(data["Description"])
     print(data["ID"])
     print(data["Completed"])
+    
